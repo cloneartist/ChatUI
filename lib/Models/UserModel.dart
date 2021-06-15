6d40a4e55458476e471704b4628c2727612fd1d0
icuriouswiz <73981868+cloneartist@users.noreply.github.com>
2021-06-15 16:50:04 +0530

    Add User Model

---
+++ lib/Models/UserModel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatUsers{
+  @required String name;
+  String messageText;
+  String imageURl;
+  String time;
+  ChatUsers({ this.name,@required this.messageText,@required this.imageURl,@required this.time})
+
+}
